e310e86a84767ce794027aa254874872beda5b9d
dehorus <ruizm.aj@gmail.com>
2021-01-21 13:09:16 -0600

    new landing page FRONTENDMENTOR

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.hamburguer-menu');
+const navbar = document.querySelector('.navbar');
+const lines = document.querySelector('.line-1', '.line-2', '.line-3');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+});
